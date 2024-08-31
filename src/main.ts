52700fd7cf111ee0a71481c927114ece56e7c477
kripsi <kripsi@kripsis-MacBook-Pro.local>
2024-08-31 04:16:20 +0300

    s

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './css/style.css';
+import './css/vars.css';
+import './css/typography.css';
+import './css/header.css';
+import './css/buttons.css';
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//   </div>
+// `
+
